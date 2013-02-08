2bfe366ed24a7f16b713b68682b0cd88dc461ec1
Marc <marc.siegel@timgroup.com>
2013-02-08 15:24:41 -0500

    Modify to download youdevise fork of Play20

--- Library/Formula/play.rb
+++ Library/Formula/play.rb
@@ -1,11 +1,12 @@
 require 'formula'
 
+# Modified to download youdevise fork
 class Play < Formula
   homepage 'http://www.playframework.org/'
-  url 'http://downloads.typesafe.com/play/2.1.0/play-2.1.0.zip'
-  sha1 '0708a30906673b5cded859b9d3d772a01855e07a'
+  url 'https://github.com/downloads/youdevise/Play20/play-2.0.5-YD1.zip'
+  sha1 '74c24f1c44f4394385b4735cff50beda30e304ef'
 
-  head 'https://github.com/playframework/Play20.git'
+  head 'https://github.com/youdevise/Play20.git'
 
   def install
     rm Dir['*.bat'] # remove windows' bat files
